96ca1966da167c0cc044675b9519feb9bc10ff11
Marshanda Febriana Putri <164144534+marshafrp@users.noreply.github.com>
2024-12-16 23:18:06 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+//import TopUp from './TopUp';
+//import Transfer from './screeens/Transfer';
+//import Login from './screeens/Login';
+//import Register from './screeens/Register';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+//registerRootComponent(TopUp);
+//registerRootComponent(Transfer);
+//registerRootComponent(Login);
+//registerRootComponent(Register);
+
+
